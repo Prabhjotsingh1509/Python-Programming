b61a98e7807f2b002aa0e65d05bb2ded1a2b124a
Prabhjot Singh <amssre.16461@gmail.com>
2026-02-25 16:51:18 +0530

    error

--- Python-Experiment/Experiment-5/6problem.py
+++
@@ -1,8 +0,0 @@
-'''Write a lambda function which gives tuple of max and min from a list. 
-Sample input: [10, 6, 8, 90, 12, 56] 
-Sample output: (90,6)'''
-
-max_min = lambda lst: (max(lst), min(lst))
-numbers = [10, 6, 8, 90, 12, 56]
-result = max_min(numbers)
-print(result)
--- Python-Experiment/Experiment-5/9problem.py
+++
@@ -1,7 +0,0 @@
-'''Write a program to create two lists and generate a dictionary with keys from list1 and 
-values from list2.'''
-list1 = ['a', 'b', 'c', 'd']
-list2 = [10, 20, 30, 40]
-result_dict = dict(zip(list1, list2))
-
-print("Generated Dictionary:", result_dict)
